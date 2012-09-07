d6d6d771a9a67e6e971921bf42880235d8a818f7
RandM <randm@fullsail.com>
2012-09-06 19:54:05 -0600

    Finalizing Gold App

---
+++ MIU/Week 2/Gold/js/json.js
@@ -0,0 +1,290 @@
+/*
+Randall Mathews
+MIU 1209
+Week 1 Project 1
+08/30/2012
+*/
+
+var json = {
+		"patient1": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "Ricky"],
+			"patientLast":      	["Last Name:", "Bobby"],
+			"patientAge":      		["Age of Patient:" , "42"],
+			"patientDate":      	["Date Added:" , "2012-08-21"],
+			"drugAllergies":     	["Drug Allergies:" , "Hot Beer"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Beer"],
+			"doseMg":     			["Dose in MG:" , "100"],
+			"comments":       		["Comments:" , "Patient needs a beer"]
+			
+		},
+		
+		"patient2": {
+			"sextype":      		["Sex Type:", "Alien"],
+			"patientFirst":  		["First Name:", "Blork"],
+			"patientLast":      	["Last Name:", "Bobby"],
+			"patientAge":      		["Age of Patient:" , "55"],
+			"patientDate":      	["Date Added:" , "2012-08-20"],
+			"drugAllergies":     	["Drug Allergies:" , "Oxygen"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Dramamine"],
+			"doseMg":     			["Dose in MG:" , "30"],
+			"comments":       		["Comments:" , "Patient suffers from air sickness"]
+			
+		},
+
+		"patient3": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Betty"],
+			"patientLast":      	["Last Name:", "Benthly"],
+			"patientAge":      		["Age of Patient:" , "54"],
+			"patientDate":      	["Date Added:" , "2012-08-19"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "none"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , ""]
+			
+		},
+
+		"patient4": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Cindy"],
+			"patientLast":      	["Last Name:", "Scott"],
+			"patientAge":      		["Age of Patient:" , "25"],
+			"patientDate":      	["Date Added:" , "2012-08-10"],
+			"drugAllergies":     	["Drug Allergies:" , "penicillin"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Lente Insulin"],
+			"doseMg":     			["Dose in MG:" , "10"],
+			"comments":       		["Comments:" , "Patient has diabetes type 2"]
+			
+		},
+
+		"patient5": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "Clay"],
+			"patientLast":      	["Last Name:", "Pinkerton"],
+			"patientAge":      		["Age of Patient:" , "53"],
+			"patientDate":      	["Date Added:" , "2012-07-20"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Pancrelipase"],
+			"doseMg":     			["Dose in MG:" , "30"],
+			"comments":       		["Comments:" , "Patient suffers from Pancreatitis"]
+			
+		},
+
+		"patient6": {
+			"sextype":      		["Sex Type:", "Alien"],
+			"patientFirst":  		["First Name:", "Klorg"],
+			"patientLast":      	["Last Name:", "Johnson"],
+			"patientAge":      		["Age of Patient:" , "155"],
+			"patientDate":      	["Date Added:" , "2011-09-22"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "none"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , "Patient is strange"]
+			
+		},
+
+		"patient7": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "John"],
+			"patientLast":      	["Last Name:", "Clark"],
+			"patientAge":      		["Age of Patient:" , "35"],
+			"patientDate":      	["Date Added:" , "2012-04-10"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "none"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , "Patient complains of headaches"]
+			
+		},
+
+		"patient8": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Gail"],
+			"patientLast":      	["Last Name:", "Chapman"],
+			"patientAge":      		["Age of Patient:" , "53"],
+			"patientDate":      	["Date Added:" , "2010-10-21"],
+			"drugAllergies":     	["Drug Allergies:" , "Peanuts"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Allegra"],
+			"doseMg":     			["Dose in MG:" , "10"],
+			"comments":       		["Comments:" , "No info"]
+			
+		},
+
+		"patient9": {
+			"sextype":      		["Sex Type:", "Unknown"],
+			"patientFirst":  		["First Name:", "Unknown"],
+			"patientLast":      	["Last Name:", "Unknown"],
+			"patientAge":      		["Age of Patient:" , "70"],
+			"patientDate":      	["Date Added:" , "2010-08-20"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Unknown"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , "Patient fail from curb"]
+			
+		},
+
+		"patient10": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Sherry"],
+			"patientLast":      	["Last Name:", "Frans"],
+			"patientAge":      		["Age of Patient:" , "35"],
+			"patientDate":      	["Date Added:" , "2011-09-25"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Kantrex"],
+			"doseMg":     			["Dose in MG:" , "30"],
+			"comments":       		["Comments:" , "Patient suffers HIV"]
+			
+		},
+
+		"patient11": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "Fred"],
+			"patientLast":      	["Last Name:", "Jones"],
+			"patientAge":      		["Age of Patient:" , "25"],
+			"patientDate":      	["Date Added:" , "2012-02-22"],
+			"drugAllergies":     	["Drug Allergies:" , "Tadalafil"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Aspirin"],
+			"doseMg":     			["Dose in MG:" , "30"],
+			"comments":       		["Comments:" , "Patient suffered chest pains"]
+			
+		},
+
+		"patient12": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "Scott"],
+			"patientLast":      	["Last Name:", "Hanes"],
+			"patientAge":      		["Age of Patient:" , "52"],
+			"patientDate":      	["Date Added:" , "2009-08-20"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Vicoprofen"],
+			"doseMg":     			["Dose in MG:" , "7.5"],
+			"comments":       		["Comments:" , "Patient suffers from a back injury"]
+			
+		},
+
+		"patient13": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "June"],
+			"patientLast":      	["Last Name:", "Cleaver"],
+			"patientAge":      		["Age of Patient:" , "75"],
+			"patientDate":      	["Date Added:" , "2008-01-10"],
+			"drugAllergies":     	["Drug Allergies:" , "Morphine"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Hydrochlorothiazide"],
+			"doseMg":     			["Dose in MG:" , "30"],
+			"comments":       		["Comments:" , "Patient is hypertensive"]
+			
+		},
+
+		"patient14": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Ann"],
+			"patientLast":      	["Last Name:", "Rule"],
+			"patientAge":      		["Age of Patient:" , "66"],
+			"patientDate":      	["Date Added:" , "2008-06-27"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "none"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , "Patient involved in MVA"]
+			
+		},
+
+		"patient15": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "Jerry"],
+			"patientLast":      	["Last Name:", "Childs"],
+			"patientAge":      		["Age of Patient:" , "39"],
+			"patientDate":      	["Date Added:" , "2007-02-15"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "none"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , "Patient involved in MVA"]
+			
+		},
+
+		"patient16": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "Jim"],
+			"patientLast":      	["Last Name:", "Pinkerston"],
+			"patientAge":      		["Age of Patient:" , "24"],
+			"patientDate":      	["Date Added:" , "2009-09-30"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "none"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , "Patient complaining of chest pain"]
+			
+		},
+
+		"patient17": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Jane"],
+			"patientLast":      	["Last Name:", "Doe"],
+			"patientAge":      		["Age of Patient:" , "85"],
+			"patientDate":      	["Date Added:" , "2012-08-20"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "none"],
+			"doseMg":     			["Dose in MG:" , "0"],
+			"comments":       		["Comments:" , "Patient unresponsive"]
+			
+		},
+
+		"patient18": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Irene"],
+			"patientLast":      	["Last Name:", "Mathers"],
+			"patientAge":      		["Age of Patient:" , "47"],
+			"patientDate":      	["Date Added:" , "2007-12-05"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Bumex"],
+			"doseMg":     			["Dose in MG:" , "30"],
+			"comments":       		["Comments:" , "Patient is hypertensive"]
+			
+		},
+
+		"patient19": {
+			"sextype":      		["Sex Type:", "Male"],
+			"patientFirst":  		["First Name:", "Adam"],
+			"patientLast":      	["Last Name:", "West"],
+			"patientAge":      		["Age of Patient:" , "77"],
+			"patientDate":      	["Date Added:" , "2011-09-29"],
+			"drugAllergies":     	["Drug Allergies:" , "none"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Alcohol"],
+			"doseMg":     			["Dose in MG:" , "100"],
+			"comments":       		["Comments:" , "Patient suffers Alcoholism"]
+			
+		},
+
+		"patient20": {
+			"sextype":      		["Sex Type:", "Female"],
+			"patientFirst":  		["First Name:", "Britney"],
+			"patientLast":      	["Last Name:", "Gomez"],
+			"patientAge":      		["Age of Patient:" , "46"],
+			"patientDate":      	["Date Added:" , "2008-08-25"],
+			"drugAllergies":     	["Drug Allergies:" , "Penicillin"],
+			"allergy":      		["Unknown Allergies:" , "none"],
+			"curMeds":     			["Current Meds:"  , "Oxycodone"],
+			"doseMg":     			["Dose in MG:" , "30"],
+			"comments":       		["Comments:" , "Patient suffers constant pain"]
+			
+		}
+};	
+
+
---
+++ MIU/Week 2/Gold/js/main.js
@@ -0,0 +1,274 @@
+/*
+Randall Mathews
+MIU 1209
+Week 1 Project 1
+08/30/2012
+*/
+
+//Wait until DOM is ready.
+window.addEventListener("DOMContentLoaded", function(){
+
+
+
+		//getElementsById Function
+		function $(x){
+			var theElement = document.getElementById(x);
+			return theElement;
+}
+
+		function makeSexTypes(){
+				var formTag = document.getElementsByTagName( "form" ),
+				selectLi = $( 'select' ),
+				makeSelect = document.createElement( 'select' );
+				makeSelect.setAttribute( "id", "sexTypes" );
+			for( var i = 0, j = sexTypes.length; i < j; i++ ){
+				var makeOption = document.createElement( 'option' );
+				var optText = sexTypes[i];
+				makeOption.setAttribute( "value", optText );
+				makeOption.innerHTML = optText;
+				makeSelect.appendChild( makeOption );
+	}
+			selectLi.appendChild(makeSelect);
+}
+
+		function storeData(key){
+			if(!key){
+				var id 						= Math.floor( Math.random() * 1000001 );
+			}else{
+				id = key;
+	
+}
+			getCheckboxValue();
+			var item 						= {};
+				item.sextype 				= ["Sex Type:" , $('sexTypes').value];
+				item.patientFirst 			= ["First Name:" , $('patientFirst').value];
+				item.patientLast 			= ["Last Name:" , $('patientLast').value];
+				item.patientAge 			= ["Age of Patient:" , $('patientAge').value];
+				item.patientDate 			= ["Date Added:" , $('patientDate').value];
+				item.drugAllergies 			= ["Drug Allergies:" , $('drugAllergies').value];
+				item.allergy 				= ["Unknown Allergies:" , allergyValue];
+				item.curMeds 				= ["Current Meds:" , $('curMeds').value];
+				item.doseMg 				= ["Dose in MG:" , $('doseMg').value];
+				item.comments 				= ["Comments:" , $('comments').value];
+
+			localStorage.setItem( id, JSON.stringify(item));
+			alert("Patient Added!");
+}
+
+		function getCheckboxValue(){
+			if($("unknown").checked){
+				allergyValue = $("unknown").value;
+			} else {
+				allergyValue = "no";
+	}
+}
+		
+		
+
+
+		function getData(){
+		toggleControls( "on" );
+			if( localStorage.length === 0 ){
+				alert( "You have no saved patients. Now loading default patient data." );
+				autoFillData();
+}
+				var makeDiv = document.createElement('div');
+				makeDiv.setAttribute("id", "items");
+				var makeList = document.createElement('ul');
+				makeDiv.appendChild( makeList );
+				document.body.appendChild( makeDiv );
+				$( 'items' ).style.display = "block";
+			for( var i = 0, j = localStorage.length; i < j;  i++ ){
+				var makeLi = document.createElement('li');
+				var linksLi =document.createElement('li');
+				makeList.appendChild(makeLi);
+				var key = localStorage.key(i);
+				var value = localStorage.getItem(key);
+				var obj = JSON.parse(value);
+				var makeSubList = document.createElement('ul');
+				makeLi.appendChild( makeSubList );
+				getImage(obj.sextype[1], makeSubList);
+			for( var n in obj ){
+				var makeSubLi = document.createElement('li');
+				makeSubList.appendChild( makeSubLi );
+				var optSubText = obj[n][0] + " " + obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
+		}
+			makeItemLinks(localStorage.key(i), linksLi);
+	}
+}
+
+			function getImage(patName, makeSubList){
+				var imageLi = document.createElement('li');
+				makeSubList.appendChild(imageLi);
+				var newImg = document.createElement('img');
+				var setSrc = newImg.setAttribute("src", "img/" + patName + ".png");
+				imageLi.appendChild(newImg);
+}			
+			
+			function autoFillData(){
+				for(var n in json){
+					var id = Math.floor(Math.random() * 1000001);
+					localStorage.setItem(id, JSON.stringify(json[n]));
+		
+	}
+}
+
+
+			function makeItemLinks(key, linksLi){
+				var editLink = document.createElement('a');
+			    editLink.href = "#";
+				editLink.key = key;
+				var editText = "Edit Patient";
+				editLink.addEventListener("click",  editItem);
+				editLink.innerHTML = editText;
+				linksLi.appendChild(editLink);
+				
+				var breakTag = document.createElement('br');
+				linksLi.appendChild(breakTag);
+				
+				
+				var deleteLink = document.createElement('a');
+				deleteLink.href = "#";
+				deleteLink.key = key;
+				var deleteText = "Delete Patient";
+				deleteLink.addEventListener("click",  deleteItem);
+				deleteLink.innerHTML = deleteText;
+				linksLi.appendChild(deleteLink);
+				
+				
+}
+
+			function editItem(){
+				var value = localStorage.getItem(this.key);
+				var item  = JSON.parse(value);
+				
+				toggleControls("off");
+				$('sexTypes').value 			= item.sextype[1];
+				$('patientFirst').value			= item.patientFirst[1];
+				$('patientLast').value 			= item.patientLast[1];
+				$('patientAge').value 			= item.patientAge[1];
+				$('patientDate').value 		= item.patientDate[1];
+				$('drugAllergies').value 		= item.drugAllergies[1];			
+				$('curMeds').value 				= item.curMeds[1];
+				$('doseMg').value 				= item.doseMg[1];
+				$('comments').value 			= item.comments[1];
+				
+				
+				save.removeEventListener("click", storeData);
+				$('submit').value = "Edit Patient";
+				var editSubmit = $('submit');
+				editSubmit.addEventListener("click", validate);
+				editSubmit.key = this.key;
+		
+}
+
+			function validate(e){
+				var getSexType = $('sexTypes');
+				var getPatientFirst = $('patientFirst');
+				var getPatientLast = $('patientLast');
+				var getDrugAllergies = $('drugAllergies');
+				
+				errMsg.innerHTML = "";
+				getSexType.style.border = "1px solid black";
+				getPatientFirst.style.border = "1px solid black";
+				getPatientLast.style.border = "1px solid black";
+				getDrugAllergies.style.border = "1px solid black";
+				
+				var messageAry = [];
+					if(getSexType.value === "<-Select Sex->"){
+						var  sexTypeError = "Please Enter Genre";
+						getSexType.style.border = "1px solid red";
+						messageAry.push(sexTypeError);
+	}
+					if(getPatientFirst.value === ""){
+							var  patientFirstError = "Please Enter First Name";
+							getPatientFirst.style.border = "1px solid red";
+							messageAry.push(patientFirstError);
+	}
+					if(getPatientLast.value === ""){
+							var  patientLastError = "Please Enter Last Name";
+							getPatientLast.style.border = "1px solid red";
+							messageAry.push(patientLastError);
+	}						
+					if(getDrugAllergies.value === ""){
+							var  drugAllergiesError = "Please Enter Drug Allergies";
+							drugAllergies.style.border = "1px solid red";
+							messageAry.push(drugAllergiesError);
+
+	}
+					if(messageAry.length >= 1) {
+						for(var i = 0,  j = messageAry.length;  i < j;  i++){
+							var txt = document.createElement('li');
+							txt.innerHTML = messageAry[i];
+							errMsg.appendChild(txt);
+	}
+	
+					e.preventDefault();
+					return false;
+			}else{
+					storeData(this.key);
+			}
+}
+
+
+			function toggleControls( n ){
+				switch( n ){
+					case "on":
+						$('patientForm').style.display = "none";
+						$('clear').style.display = "inline";
+						$('display').style.display = "none";
+						$('addNew').style.display = "inline";
+						break;
+
+					case "off":
+						$('patientForm').style.display = "block";
+						$('clear').style.display = "inline";
+						$('display').style.display = "inline";
+						$('addNew').style.display = "none";
+						$('items').style.display = "none";
+						break;
+
+			default:
+				return false;	
+	}
+}
+
+			function deleteItem(){
+				var ask = confirm("Are you sure you want to delete this patient?");
+				if(ask){
+					localStorage.removeItem(this.key);
+					alert("Patient was deleted!");
+					window.location.reload();
+				}else{	
+						alert("Patient was NOT deleted.");
+	}
+}
+
+											
+								
+			function clearData(){
+				if(localStorage.length === 0){
+					alert( "You have no saved patients." );
+				}else{
+					localStorage.clear();
+					alert( "All patients have been deleted!" );
+					window.location.reload();
+				return false;
+	}
+}
+
+				var sexTypes = [ "<-Select Sex->", "Male", "Female", "Unknown", "Alien" ];
+				makeSexTypes();
+				errMsg = $('errors');
+				
+
+				var displayLink = $('display');
+				displayLink.addEventListener("click",  getData);
+				var clearLink = $('clear');
+				clearLink.addEventListener("click",  clearData);
+				var save = $( 'submit' );
+				save.addEventListener("click",  validate);
+
+});
