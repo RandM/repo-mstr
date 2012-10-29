e834be112d961b3ddddd9ba9dc3d06f38ae8b7fd
RandM <randm@fullsail.com>
2012-10-29 03:03:30 -0600

    Refactoring and storage with console.log

--- MIU/Week 4/Gold/js/main2.js
+++ MIU/Week 4/Gold/js/main2.js
@@ -1,4 +1,21 @@
+/*
+Randall Mathews
+ASD 1210
+Week 1 Project 1
+10/25/2012
+*/
 
+
+
+$('#index').on('pageinit', function(){
+	//code needed for home page goes here
+		$('#Male').on('click', getData);
+		$('#Female').on('click', getData);
+		$('#Unknown').on('click', getData);
+		$('#Alien').on('click', getData);
+		$('#clearAllData').on('click', clearData);
+	
+});	
 
 
 		
@@ -7,44 +24,29 @@ $(document).on('pageinit', function(){
 		var myForm = $('#addPatientForm');
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
-				console.log(validator)
+				
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
-			patientInfo(data);
+			storeData(data);
+		console.log(data)
 		}
 	});
 	
 
 	//any other code needed for addItem page goes here
 	
-});
+
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
-var patientInfo = function(data){
-	console.log(data);
 
-};
+ var storeData = function(data, key){
+			if(!key){
+				var id 						= Math.floor( Math.random() * 1000001 );
+			}else{
+				id = key;
 
+  	}	
 
-var autofillData = function (){
-	 
-};
-
-var getData = function(){
-
-};
-
-var storeData = function(data){
-	
-}; 
-
-var	deleteItem = function (){
-			
-};
-					
-var clearLocal = function(){
-
-};
-
-
+  }
+});
