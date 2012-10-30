bbcd9ed0b74f00e62407e2d5d684bc26f9eb6bd5
RandM <randm@fullsail.com>
2012-10-29 21:41:45 -0600

    Still playing catch-up, adding edit, delete JQM page

--- ASD/Week 1/Gold/js/main2.js
+++ ASD/Week 1/Gold/js/main2.js
@@ -39,7 +39,7 @@ $(document).on('pageinit', function(){
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
-
+//Store Data to Local Storage(use console.log if data not displayed)
  var storeData = function(data, key){
 			if(!key){
 				var id 						= Math.floor( Math.random() * 1000001 );
@@ -49,4 +49,46 @@ $(document).on('pageinit', function(){
   	}	
 
   }
+
+//display data in local storage
+  var getData = function(){
+		
+			if( localStorage.length === 0 ){
+				alert( "You have no saved patients. Now loading default patient data." );
+				autoFillData();
+}
+				var makeDiv = document.createElement('div');
+				makeDiv.setAttribute("id", "items");
+				var makeList = document.createElement('ul');
+				makeDiv.appendChild( makeList );
+				document.body.appendChild( makeDiv );
+				$( 'items' ).style.display = "block";
+			for( var i = 0, j = localStorage.length; i < j;  i++ ){
+				var makeLi = document.createElement('li');
+				var linksLi =document.createElement('li');
+				makeList.appendChild(makeLi);
+				var key = localStorage.key(i);
+				var value = localStorage.getItem(key);
+				var obj = JSON.parse(value);
+				var makeSubList = document.createElement('ul');
+				makeLi.appendChild( makeSubList );
+				getImage(obj.sextype[1], makeSubList);
+			for( var n in obj ){
+				var makeSubLi = document.createElement('li');
+				makeSubList.appendChild( makeSubLi );
+				var optSubText = obj[n][0] + " " + obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
+		}
+			makeItemLinks(localStorage.key(i), linksLi);
+	}
+}
+
+
+
+
+
+
+
+
 });
