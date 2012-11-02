bb38bdfc4abc579a1815b0e8f5ecb257d7f4370e
RandM <randm@fullsail.com>
2012-11-02 14:33:07 -0600

    Complete for ASD week 2, still need to get edit, delete working

--- ASD/Week 2/Gold/js/main2.js
+++ ASD/Week 2/Gold/js/main2.js
@@ -7,61 +7,180 @@ Week 2 Project 2
 
 
 
-$('#index').on('pageinit', function(){
-	//code needed for home page goes here
-		$('#Male').on('click', getData);
-		$('#Female').on('click', getData);
-		$('#Unknown').on('click', getData);
-		$('#Alien').on('click', getData);
-		$('#clearAllData').on('click', clearData);
-	
-});	
 
 
-		
+
+        
 $(document).on('pageinit', function(){
 
-		var myForm = $('#addPatientForm');
-		    myForm.validate({
-			invalidHandler: function(form, validator) {
-				
-			},
-			submitHandler: function() {
-		var data = myForm.serializeArray();
-			storeData(data);
-		
-		}
-		
-	});
-	
+        var myForm = $('#addPatientForm');
+            myForm.validate({
+            invalidHandler: function(form, validator) {
+                
+            },
+            submitHandler: function() {
+        var data = myForm.serializeArray();
+            storeData(data);
+        
+        }
+        
+    });
+    
 
-	//any other code needed for addItem page goes here
-	
+    //any other code needed for addItem page goes here
+    
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
 //Store Data to Local Storage(use console.log if data not displayed)
  
  var storeData = function(data, key){
-			var id = localStorage.length === 0;
-				localStorage.setItem(id, JSON.stringify(data));
-				alert("Patient Saved");
+            var id = localStorage.length === 0;
+                localStorage.setItem(id, JSON.stringify(data));
+                alert("Patient Saved");
 
 
  }
 
 
 var clearData = function(data, key){
-				var id = localStorage.length === 0;
-				localStorage.clear();
-				alert("Local Storage Has Been Cleared");
-	
+                var id = localStorage.length === 0;
+                localStorage.clear();
+                alert("Local Storage Has Been Cleared");
+    
 }
  
 
 
 
 
-
-
 });
+
+
+
+//week 2 Stuff
+var dataIn = $("#dataIn");
+var dataOut = $("#dataOut");
+
+$("#JBtn").on("click", changeToJSON);
+$("#XBtn").on("click", changeToXML);
+$("#CBtn").on("click", changeToCSV);
+
+    function changeToJSON(){
+
+        dataIn.html("JSON Data");
+        $.ajax({
+                url:"xhr/data.json",
+                type:"GET",
+                dataType: "json",
+                success: parseJson     
+ });
+    
+    function parseJson(data){
+
+        console.log("JSON Data");
+        console.log(data);
+        
+        dataOut.html("");
+        $.each(data,
+        function(index, element){
+            
+            var li = $("<li></li>");
+            li.appendTo(dataOut);
+            
+            var p = $("<p></p>");
+            p.html(element.patientFirst[0] + " " + element.patientFirst[1] + "<br>"
+                   + element.patientLast[0] + " " + element.patientLast[1] + "<br>"
+                   + element.patientAge[0] + " " + element.patientAge[1] + "<br>"
+                   + element.patientDate[0] + " " + element.patientDate[1] +"<br>"
+                   + element.drugAllergies[0] + " " + element.drugAllergies[1]); 
+            p.appendTo(li);
+        }
+      ); 
+   }
+}
+
+    function changeToXML(){
+
+        dataIn.html("XML Data");
+        $.ajax({
+                url:"xhr/data.xml",
+                type:"GET",
+                dataType:"xml",
+                success: parseXData    
+    });
+    
+    function parseXData(data){
+
+        console.log("XML Data");
+        console.log(data);
+        
+        dataOut.html("");
+        
+        $(data).find("patient").each(
+            function(){
+                
+                var li = $("<li></li>");
+                li.appendTo(dataOut);
+                
+                var p = $("<p></p>");
+                p.appendTo(li);
+                
+                var cur = $(this);
+                p.append("patientFirst: " + findString("patientFirst")
+                         + "patientLast: " + findString("patientLast")
+                         + "patientAge: " + findString("patientAge")
+                         + "patientDate: " + findString("patientDate")
+                         + "drugAllergies: " + findString("drugAllergies")
+                        );
+                
+
+                function findString(string){
+                    
+                    return cur.find(string).text() + "<br>";
+                }
+            }
+        );
+    }
+}
+
+    function changeToCSV(){
+
+        dataIn.html("CSV Data");
+        $.ajax(
+            {
+                type:"GET",
+                url:"xhr/data.csv",
+                dataType:"text",
+                success: parseCsv
+            }
+    );
+        
+        function parseCsv(data){
+
+            console.log("CSV Data");
+            console.log(data);
+            
+            dataOut.html("");
+            
+            //CSV Lib
+            var chars = $.csv.toArrays(data);
+            
+            
+            for (var outer = 1; outer < chars.length; outer++)
+            {
+                var li = $("<li></li>");
+                li.appendTo(dataOut);
+                
+                var p = $("<p></p>");
+                p.appendTo(li);
+                
+                for (var inner = 0; inner < chars[0].length; inner++)
+                {
+                    p.append(chars[0][inner] + ": " + chars[outer][inner]);
+                    p.append("<br>")
+                }
+            }
+        }
+    }
+
